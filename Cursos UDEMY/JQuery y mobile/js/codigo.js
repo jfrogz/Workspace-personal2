623aa6d3a84904f992aa342b86b37b65268d2481
Fernando R <sr.frog@hotmail.com>
2017-09-12 19:20:59 -0500

    Cambios para porobar ion.sound plugin

--- Cursos UDEMY/JQuery y mobile/js/codigo.js
+++ Cursos UDEMY/JQuery y mobile/js/codigo.js
@@ -7,8 +7,24 @@ function inicio() {
 
 function onDesaparece() {
     $("#afrodita").hide();
+    ion.sound.play("beer_can_opening");
 }
 
 function fondo() {
     $("#titulo").css("background", "red");
 }
+
+ion.sound({
+    sounds: [
+        {name: "beer_can_opening"},
+        {name: "bell_ring"},
+        {name: "branch_break"},
+        {name: "button_click"}
+    ],
+
+    // main config
+    path: "static/sounds/",
+    preload: true,
+    multiplay: true,
+    volume: 0.9
+});
